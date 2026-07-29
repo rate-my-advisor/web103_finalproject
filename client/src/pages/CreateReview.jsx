0725fd91cf1f7402dd8f900f2c37f0edfae22543
Kaylie Chang <firenachang@gmail.com>
2026-07-29 21:19:00 +0800

    feat: add create review button in advisor profile page
    - Just looks for now, functionality not yet implemented
    - Created CreateReview.js and .css
    - Added (but commented out) the path for the create review page in App.jsx

---
+++ client/src/pages/CreateReview.jsx
@@ -0,0 +1,11 @@
+// pages/CreateReview.jsx
+import ReviewForm from "../components/ReviewForm";
+
+export default function CreateReview() {
+  return (
+    <main>
+      <h1>Review Advisor</h1>
+      <ReviewForm />
+    </main>
+  );
+}
