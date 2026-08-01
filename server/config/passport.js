ff2d4669b800dd78f94bd6bd93ac88f3b403976d
Pushyboy <jwong47@u.rochester.edu>
2026-08-01 00:36:24 -0400

    Minor syntax error

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,7 +6,7 @@ const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
 const CALLBACK_URL = process.env.GITHUB_CALLBACK_URL
 
-options = {
+const options = {
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
   callbackURL: CALLBACK_URL
